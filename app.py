90d5f9104e51bafed450ee9b2e0bbcfb03acb7b7
Mucmell <arducmucahit@gmail.com>
2018-10-18 11:48:19 +0000

    s

--- app.py
+++ app.py
@@ -17,12 +17,12 @@ ASK_VERIFY_TIMESTAMP_DEBUG = True
 def main_function():
     return dress_recommender()
 
-	
+
 @ask.intent("AMAZON.StopIntent")
 def stop_function():
     return statement("See you tomorrow")
 
-	
+
 @ask.intent("AMAZON.CancelIntent")
 def cancel_function():
     return statement("See you tomorrow")
@@ -118,8 +118,8 @@ if __name__ == '__main__':
         verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
         if verify == 'false':
             app.config['ASK_VERIFY_REQUESTS'] = False
-	app.config['ASK_APPLICATION_ID'] = 'amzn1.ask.skill.2d740502-1149-44a2-98d2-3544fc24cb72'
-	app.config['ASK_VERIFY_REQUESTS'] = True
-	app.config['ASK_VERIFY_TIMESTAMP_DEBUG'] = True
+    app.config['ASK_APPLICATION_ID'] = 'amzn1.ask.skill.2d740502-1149-44a2-98d2-3544fc24cb72'
+    app.config['ASK_VERIFY_REQUESTS'] = True
+    app.config['ASK_VERIFY_TIMESTAMP_DEBUG'] = True
     app.run(debug=True)
 
